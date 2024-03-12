c7b0f3fa27f468481f300d87612dc9b97a17881f
gaji12345 <gajimax12345@gmail.com>
2024-03-12 13:48:22 +0530

    implement loginmethod in loggincontroller

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -2,19 +2,27 @@ package controller;
 
 import bo.custom.LoginBo;
 import bo.impl.LoginBoImpl;
+import db.DBConnection;
 import dto.Logindto;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class LoginFormController {
     public ImageView root;
     public TextField txtUserName;
-    public TextField txtpassword;
+  //  public TextField txtpassword;
     public ComboBox cmbPostions;
     public PasswordField txtPassFld;
     public Button btnLogin;
@@ -38,6 +46,7 @@ public class LoginFormController {
     }
 
     public void pw_OnAction(MouseEvent mouseEvent) {
+
     }
 
     public void chk_boxOnAction(MouseEvent mouseEvent) {
@@ -45,5 +54,51 @@ public class LoginFormController {
 
 
     public void login_OnAction(ActionEvent actionEvent) {
+        String title = (String) cmbPostions.getValue();
+        String userName = txtUserName.getText();
+        String password = txtPassFld.getText();
+        try {
+            Connection conn = DBConnection.getInstance().getConnection();
+            PreparedStatement stm = conn.prepareStatement("SELECT * FROM login WHERE title = ? AND userName = ? AND password = ?");
+            stm.setString(1, title);
+            stm.setString(2, userName);
+            stm.setString(3, password);
+            ResultSet resultSet = stm.executeQuery();
+
+            if (resultSet.next()) {
+                // Login successful, load appropriate form
+                String path = "/view/";
+                switch(title) {
+                    case "Manager":
+                        path += "DashBoard.fxml";
+                        break;
+                    case "Receptionist":
+                        path += "receptionist_form.fxml";
+                        break;
+                    // add more cases as needed for other user types
+                }
+                Stage stage = (Stage) root.getScene().getWindow();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(path))));
+                stage.setTitle("Manage");
+                stage.centerOnScreen();
+                stage.show();
+
+            } else {
+                // Login failed, show error message
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Login Failed");
+                alert.setHeaderText(null);
+                alert.setContentText("Invalid username or password!");
+                alert.showAndWait();
+            }
+        } catch (SQLException | IOException ex) {
+            ex.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("An error occurred while trying to login!");
+            alert.showAndWait();
+        }
     }
-}
+    }
+
