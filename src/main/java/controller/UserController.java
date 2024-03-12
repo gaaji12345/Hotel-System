554d484e2d96ffec898fbe546a9e8a12c55554f6
gaji12345 <gajimax12345@gmail.com>
2024-03-12 13:04:15 +0530

    implement userbo save method

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class UserController {
+
+    public AnchorPane root;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtPassword;
+    public TextField txtPosstion;
+    public Button btnSave;
+    public Button btnUpdate;
+    public Button btnDelete;
+    public TableView tblMian;
+    public TableColumn cblid;
+    public TableColumn cblName;
+    public TableColumn cblPassword;
+    public TableColumn cblpos;
+
+    public void saveON(ActionEvent actionEvent) {
+    }
+
+    public void updateON(ActionEvent actionEvent) {
+    }
+
+    public void deleteOn(ActionEvent actionEvent) {
+    }
+}
