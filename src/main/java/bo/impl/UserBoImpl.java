b9cb1495180185563d3f0d079a1e51dbaa28a11d
gaji12345 <gajimax12345@gmail.com>
2024-03-12 13:02:01 +0530

    implemnt userbo delete method

---
+++ src/main/java/bo/impl/UserBoImpl.java
@@ -0,0 +1,38 @@
+package bo.impl;
+
+import bo.custom.UserBo;
+import dao.custom.UserDao;
+import dao.impl.UserDaoImpl;
+import dto.Userdto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserBoImpl implements UserBo {
+
+    UserDao userDao=new UserDaoImpl();
+    @Override
+    public boolean deleteUser(String id) throws SQLException {
+        return userDao.delete(id);
+    }
+
+    @Override
+    public boolean updateUser(Userdto dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean addUser(Userdto dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Userdto searchUser(String id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Userdto> getAllUsers() throws SQLException {
+        return null;
+    }
+}
