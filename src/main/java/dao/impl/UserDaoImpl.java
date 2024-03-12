51d0f1a135e5c311a8b5dbc80083b23dd353aa57
gaji12345 <gajimax12345@gmail.com>
2024-03-12 11:41:48 +0530

    implement delete method

---
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -0,0 +1,41 @@
+package dao.impl;
+
+import dao.custom.UserDao;
+import dao.util.SqlUtil;
+import entity.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public boolean delete(String id) throws SQLException {
+        return SqlUtil.execute("DELETE FROM user WHERE userId = ?", id);
+    }
+
+    @Override
+    public boolean update(User entity) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean add(User entity) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public User search(String id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<User> getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+}
