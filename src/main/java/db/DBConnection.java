c5b8ebd24e49f7ee00e395935a69da3ec88448e7
gaji12345 <gajimax12345@gmail.com>
2024-03-12 09:12:13 +0530

    create dbConnection

---
+++ src/main/java/db/DBConnection.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/ravana",
+                "root",
+                "Prabath550@"
+        );
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        /*if(dbConnection == null) {
+            dbConnection = new DBConnection();
+            return dbConnection;
+        } else {
+            return dbConnection;
+        }*/
+        return (null == dbConnection) ? dbConnection = new DBConnection()
+                : dbConnection;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+}
