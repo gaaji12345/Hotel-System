8d76daeb67633e392728afe7391365308fb82a90
gaji12345 <gajimax12345@gmail.com>
2024-03-14 12:41:02 +0530

    implemet save and update method in roomdaoimpl

--- src/main/java/dao/impl/RoomDaiImpl.java
+++ src/main/java/dao/impl/RoomDaiImpl.java
@@ -16,12 +16,12 @@ public class RoomDaiImpl implements RoomDao {
 
     @Override
     public boolean update(Room entity) throws SQLException {
-        return false;
+        return SqlUtil.execute("UPDATE Room SET roomDetails = ?, roomType = ?, roomPrice = ? WHERE roomId = ?", entity.getDetails(), entity.getRoomType(), entity.getPrice(), entity.getId());
     }
 
     @Override
     public boolean add(Room entity) throws SQLException {
-        return false;
+        return SqlUtil.execute("INSERT INTO room(roomId, roomDetails, roomType, roomPrice) VALUES(?, ?, ?, ?)", entity.getId(), entity.getDetails(), entity.getRoomType(), entity.getPrice());
     }
 
     @Override
