16dda2de889852e1345c6a87ce1b76bb882ddb48
gaji12345 <gajimax12345@gmail.com>
2024-03-14 21:01:35 +0530

    delete method implement RoomBoImpl

---
+++ src/main/java/bo/impl/RoomBoImpl.java
@@ -0,0 +1,49 @@
+package bo.impl;
+
+import bo.custom.RoomBo;
+import dao.custom.RoomDao;
+import dao.impl.RoomDaiImpl;
+import dto.Roomdto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class RoomBoImpl  implements RoomBo {
+    RoomDao roomDao=new RoomDaiImpl();
+
+
+    @Override
+    public boolean deleteRoom(String id) throws SQLException {
+        return roomDao.delete(id);
+    }
+
+    @Override
+    public boolean updateRoom(Roomdto dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean addRoom(Roomdto dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Roomdto searchRoom(String id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Roomdto> getAllRooms() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String generateNewRoomID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void releaseRoom(String roomId, String release) throws SQLException {
+
+    }
+}
